1ce980548be36eb95baaec5c4506e0a7d04efed1
jasonchanhk <chanjky97@gmail.com>
2022-07-25 11:48:36 +0100

    connect to db & create get n post method

--- server/controllers/habits.js
+++ server/controllers/habits.js
@@ -1,14 +1,54 @@
-// GET AllHabits
-// GET '/'
+const express = require('express');
+const router = express.Router();
 
-// GET findHabitByUserId
-// POST '/:userid'
+const habitModel = require('../models/habit')
 
 // POST createNewHabit
 // POST '/'
+
+router.post("/", async (request, response) => {
+
+    console.log(request.body)
+    const { userId, id, title, description, freq, targetFreq, complete, streak } = request.body
+
+    try {
+        const habit = await habitModel.create({
+            userId: userId,
+            id: id,
+            title: title,
+            description: description,
+            freq: freq,
+            targetFreq: targetFreq,
+            complete: complete,
+            streak: streak
+        });
+        response.send(habit);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+// GET AllHabits
+// GET '/'
+
+router.get("/", async (request, response) => {
+    const habits = await habitModel.find({});
+
+    try {
+        response.send(habits);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+
+// GET findHabitByUserId
+// GET '/:userid'
 
 // PATCH UpdateHabitById
 // PATCH '/:id'
 
 // DELETE DestroyHabitById
 // DELETE '/:id')
+
+module.exports = router
